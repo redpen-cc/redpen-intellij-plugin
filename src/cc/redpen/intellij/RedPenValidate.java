79a406ad3b9ead9e4a535d66ba8ffcf3ebf42f09
Anton Keks <anton@codeborne.com>
2016-02-03 17:56:47 +0200

    let validate action display the currently open file's text

--- src/cc/redpen/intellij/RedPenValidate.java
+++ src/cc/redpen/intellij/RedPenValidate.java
@@ -2,14 +2,16 @@ package cc.redpen.intellij;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
 
 public class RedPenValidate extends AnAction {
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getData(PlatformDataKeys.PROJECT);
-        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
-        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+        PsiFile file = event.getData(LangDataKeys.PSI_FILE);
+        Messages.showMessageDialog(project, file.getText(), file.getName(), Messages.getInformationIcon());
     }
 }
