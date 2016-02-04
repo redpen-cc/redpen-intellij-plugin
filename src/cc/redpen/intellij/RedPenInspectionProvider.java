1c722bfa3fd1883437663bb75c2c1f404bd853e3
Anton Keks, Dmitri Ess <anton@codeborne.com, dmitri.ess@codeborne.com>
2016-02-04 15:04:21 +0200

    rename RedPenProvider

--- src/cc/redpen/intellij/RedPenInspectionProvider.java
+++ src/cc/redpen/intellij/RedPenInspectionProvider.java
@@ -2,7 +2,7 @@ package cc.redpen.intellij;
 
 import com.intellij.codeInspection.InspectionToolProvider;
 
-public class RedPenProvider implements InspectionToolProvider {
+public class RedPenInspectionProvider implements InspectionToolProvider {
   public Class[] getInspectionClasses() {
     return new Class[] { RedPenInspection.class};
   }
