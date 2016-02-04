30d8b30924a469f17daa0476650b0e0fee34ba97
Anton Keks, Dmitri Ess <anton@codeborne.com, dmitri.ess@codeborne.com>
2016-02-04 11:23:21 +0200

    added ivy for dependencies

--- src/cc/redpen/intellij/RedPenValidate.java
+++ src/cc/redpen/intellij/RedPenValidate.java
@@ -1,5 +1,6 @@
 package cc.redpen.intellij;
 
+import cc.redpen.RedPen;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
@@ -13,7 +14,7 @@ public class RedPenValidate extends AnAction {
         Project project = event.getData(PlatformDataKeys.PROJECT);
         PsiFile file = event.getData(LangDataKeys.PSI_FILE);
         if (file == null)
-            Messages.showMessageDialog(project, "No file currently open", "RedPen", Messages.getInformationIcon());
+            Messages.showMessageDialog(project, "No file currently open", "RedPen " + RedPen.VERSION, Messages.getInformationIcon());
         else
             Messages.showMessageDialog(project, file.getText(), file.getName(), Messages.getInformationIcon());
     }
