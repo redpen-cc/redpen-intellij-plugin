f31de274d7353acf068ad8fc1091c0e91b7eb963
Anton Keks <anton@codeborne.com>
2016-02-09 17:17:14 +0200

    rewrite download-idea in ant script

--- download_idea.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-# Downloads the latest Intellij IDEA Community jars required for script builds
-
-JSON=`wget -qO - 'https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release'`
-URL=`node -p -e "($JSON).IIC[0].downloads.linux.link"`
-wget -O - --no-check-certificate $URL | tar zxv --wildcards --no-wildcards-match-slash 'idea-IC-*/lib/*.jar'
-mv idea-IC-* idea
