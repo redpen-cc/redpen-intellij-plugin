5d856a21afac231d772eb1e461129c15a3e91f26
Anton Keks <anton@codeborne.com>
2016-02-09 15:19:48 +0200

    download first and then extract - doesn't work in travis with pipe

--- download_idea.sh
+++ download_idea.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 # Downloads the latest Intellij IDEA Community jars required for script builds
 
 JSON=`wget -qO - 'https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release'`
 URL=`node -p -e "($JSON).IIC[0].downloads.linux.link"`
-wget -qO - $URL | tar zxv --wildcards --no-wildcards-match-slash 'idea-IC-*/lib/*.jar'
+wget -O idea.tar.gz $URL
+tar zxvf idea.tar.gz --wildcards --no-wildcards-match-slash 'idea-IC-*/lib/*.jar'
 mv idea-IC-* idea
