807130c30e5a8c8f94e24cff2c6d28fc628e8097
Anton Keks <anton@codeborne.com>
2016-02-09 14:37:47 +0200

    idea download script for travis

---
+++ download_idea.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Downloads the latest Intellij IDEA Community jars required for script builds
+
+JSON=`wget -qO - 'https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release'`
+URL=`node -p -e "($JSON).IIC[0].downloads.linux.link"`
+wget -qO - $URL | tar zxv --wildcards --no-wildcards-match-slash 'idea-IC-*/lib/*.jar'
+mv idea-IC-* idea
